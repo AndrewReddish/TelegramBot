6fd05c970a92d828047ef6f29ee60da3b2a236c9
Andrew Reddish <8884515+AndrewReddish@users.noreply.github.com>
2019-10-29 10:18:39 +0200

    Add files via upload

--- andrew_bot/src/main/java/MainClass.java
+++ andrew_bot/src/main/java/MainClass.java
@@ -1,18 +1,24 @@
 import org.telegram.telegrambots.ApiContextInitializer;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
-
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
 
 public class MainClass {
+    //private static final Logger logger = LogManager.getLogger(MainClass.class);
     public static void main(String[] args) {
-
+        System.out.println("test");
         ApiContextInitializer.init();
+
+
+
         TelegramBotsApi telegramBotsApi = new TelegramBotsApi();
         try {
             telegramBotsApi.registerBot(new MyBot());
-
+            //logger.trace("Entering application.");
         } catch (TelegramApiException e) {
             e.printStackTrace();
+
         }
     }
 }
--- andrew_bot/src/main/java/MyBot.java
+++ andrew_bot/src/main/java/MyBot.java
@@ -1,10 +1,55 @@
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.BotApiMethod;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.methods.send.SendSticker;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+import java.util.ArrayList;
 
 public class MyBot extends TelegramLongPollingBot {
 
     public void onUpdateReceived(Update update) {
+        /*System.out.println(update.getMessage().getText());
+        System.out.println(update.getMessage().getFrom().getFirstName());*/
 
+        String command = update.getMessage().getText();
+        ArrayList<String> requests = new ArrayList<String>();
+        SendMessage message = new SendMessage();
+
+        if (command.equals("/myname")) {
+            System.out.println(update.getMessage().getFrom().getFirstName()
+            +update.getMessage().getFrom().getLastName());
+
+            message.setText(update.getMessage().getFrom().getFirstName() + " "
+            + update.getMessage().getFrom().getLastName());
+        } else if (command.equals("/myfirstname")) {
+            System.out.println(update.getMessage().getFrom().getFirstName());
+
+            message.setText(update.getMessage().getFrom().getFirstName());
+        } else if (command.equals("/mylastname")) {
+            System.out.println(update.getMessage().getFrom().getLastName());
+
+            message.setText(update.getMessage().getFrom().getLastName());
+        } else if (command.equals("Hi") || command.equals("Hello") || command.equals("Hey") || command.equals("Привет")) {
+            message.setText("Hi, " + update.getMessage().getFrom().getFirstName());
+        } else if (command.contains("Time") || command.contains("time")) {
+            message.setText("It's time to eat!");
+            SendSticker sticker = new SendSticker();
+        } else {
+            message.setText("What do you want, human?");
+        }
+
+        message.setChatId(update.getMessage().getChatId());
+
+
+        try {
+            execute(message);
+            requests.add(command);
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+        System.out.println(requests);
     }
 
     public String getBotUsername() {
